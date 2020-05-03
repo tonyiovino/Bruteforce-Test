1960c59b8504d744bab44d73c00973550e493b42
tonyiovino <antonioiovino2004tony@gmail.com>
2020-05-03 23:20:16 +0000

    Update

--- bruteforce.py
+++ bruteforce.py
@@ -1,25 +1,31 @@
-chiave = 999877
+f =open(".ijdg.dat", "r")
+string = f.readline()
+ijdg = int(string)
 
 print("Contenuto bloccato.")
 tentativo = input("Inserire la chiave: ")
 
-if (tentativo == chiave):
-    print("Contenuto sbloccato.\nChiave inerita GIUSTA!")
+if (tentativo == ijdg):
+    print("Contenuto sbloccato.\nChiave inserita GIUSTA!")
 
 else:
     print("Contenuto bloccato.\nChiave inserita SBAGLIATA!")
     print("1: Avvia il bruteforce (Trova la chiave);\n2: Lascia perdere ed esci dal programma.")
     scelta = input()
 
+
+
     if (scelta == 1):
         for tentativo in range(0, 999999):
-            if tentativo == chiave:
+            if tentativo == ijdg:
                 break
-        if tentativo == chiave:
+        if tentativo == ijdg:
             print("Chiave: ", tentativo)
 
         else:
             print("Nessuna chiave trovata.")
+
+    
 
     elif (scelta == 2):
         print("Arrivederci.")
